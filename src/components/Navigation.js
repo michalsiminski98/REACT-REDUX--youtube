f043eeb86a7f76f1ce998c9097bba2a6fde6c8d8
Michal Siminski <michal.siminski3@gmail.com>
2021-03-15 14:38:47 +0100

    add navigation actions

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,52 @@
+import { useState } from 'react';
+
 import '../styles/navigation.scss'
 
 const Navigation = () => {
+
+  const [notificationActive, setNotificationActive] = useState(false);
+  const [boardActive, setBoardActive] = useState(false);
+  const [submitActive, setSubmitActive] = useState(false);
+  const [searchActive, setSearchActive] = useState(false);
+
+  const handleBellClick = () => {
+    notificationActive === false ? 
+    document.querySelector('.navigation__bell-messsage').style.display='block' :
+    document.querySelector('.navigation__bell-messsage').style.display='none';
+    setNotificationActive(!notificationActive);
+  }
+
+  const handleBoardClick = () => {
+    boardActive === false ? 
+    document.querySelector('.navigation__board-messsage').style.display='block' :
+    document.querySelector('.navigation__board-messsage').style.display='none';
+    setBoardActive(!boardActive);
+  }
+
+  const handleSubmitClick = () => {
+    submitActive === false ? 
+    document.querySelector('.navigation__submit-messsage').style.display='block' :
+    document.querySelector('.navigation__submit-messsage').style.display='none';
+    setSubmitActive(!submitActive);
+  }
+
+  const handleSearchActivityClick = () => {
+    if(searchActive === false){ 
+    document.querySelector('.navigation__leftSide').style.display='none';
+    document.querySelector('.navigation__rightSide').style.display='none';
+    document.querySelector('.navigation__mobileSearch').style.display='flex';
+    }else{
+      document.querySelector('.navigation__leftSide').style.display='flex';
+      document.querySelector('.navigation__rightSide').style.display='flex';
+      document.querySelector('.navigation__mobileSearch').style.display='none';
+    }
+    setSearchActive(!searchActive);
+  }
+
   return ( 
     <nav className='navigation'>
+      {/* <div className="navigation__barBackground">
+      </div> */}
       <div className="navigation__leftSide">
         <div className="navigation__bar">
         <i className="fas fa-bars navigation__barIcon"></i>
@@ -10,14 +54,35 @@ const Navigation = () => {
         <img className="navigation__logo" src="https://logofirmy.net/wp-content/uploads/2020/04/YouTube-Logo.png" alt="youtube logo"/>
       </div>
       <div className="navigation__rightSide">
-      <input type="text" placeholder="Szukaj" className="navigation__searchInput"/>
-      <button className="navigation__searchButton">
-        <i className="fas fa-search navigation__searchIcon"></i>
-      </button>
-      <i className="fas fa-share navigation__submit"></i>
-      <i className="fas fa-chess-board navigation__board"></i>
-      <i className="fas fa-bell navigation__bell"></i>
-      <img className="fas fa-bell navigation__profile" src="https://johannesippen.com/img/blog/humans-not-users/header.jpg" alt="profilePicture"/>
+        <i className="fas fa-search navigation__searchIconMobile" onClick={handleSearchActivityClick}></i>
+        <input type="text" placeholder="Szukaj" className="navigation__searchInput"/>
+        <button className="navigation__searchButton">
+          <i className="fas fa-search navigation__searchIcon"></i>
+        </button>
+        <i className="fas fa-share navigation__submit" onClick={handleSubmitClick}>
+          <div className="navigation__submit-messsage">
+            <h2 className= "navigation__h2"><i className="fab fa-youtube"></i> Prześlij film</h2>
+          </div>
+        </i>
+        <i className="fas fa-chess-board navigation__board" onClick={handleBoardClick}>
+          <div className="navigation__board-messsage">
+            <h2 className= "navigation__h2"><i className="fab fa-youtube"></i> YouTube TV</h2>
+          </div>
+        </i>
+        <i className="fas fa-bell navigation__bell" onClick={handleBellClick}>
+          <div className="navigation__bell-messsage">
+            <h2 className= "navigation__h2">Powiadomienia</h2>
+            <hr/>
+          </div>
+        </i>
+        <img className="fas fa-bell navigation__profile" src="https://johannesippen.com/img/blog/humans-not-users/header.jpg" alt="profilePicture"/>
+      </div>
+      <div className="navigation__mobileSearch">
+        <i className="fas fa-arrow-circle-left navigation__back" onClick={handleSearchActivityClick}></i>
+        <input type="text" placeholder="Szukaj" className="navigation__searchInputMobile"/>
+        <button className="navigation__searchButtonMobile">
+          <i className="fas fa-search navigation__searchIconMobile"></i>
+        </button>
       </div>
     </nav>
    );
