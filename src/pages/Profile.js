0ca150424c255d564932e77e8474e02d8899334d
Michal Siminski <michal.siminski3@gmail.com>
2021-04-14 13:23:00 +0200

    rwd profile site

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,14 @@
 const Profile = () => {
   return ( 
-    <div>
-      profile
-    </div>
+    <section className="profile">
+      <form className="profile__form">
+        <label for="email" className="profile__label">Email:</label><br/>
+        <input type="text" id="email" name="email" className="profile__input" /><br/>
+        <label for="password" className="profile__label">Password:</label><br/>
+        <input type="password" name="password" className="profile__input" /><br/>
+        <input type="submit" value="Submit" className="profile__submit"/>
+      </form>
+    </section>
    );
 }
  
